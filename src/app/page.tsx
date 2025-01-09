5b4f0a4adffd6f31f0c38c29464fcb14f5277ed0
Satyam Sagar <satyamsagar30@gmail.com>
2025-01-10 00:48:17 +0530

    auth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
 import { Button } from "@/components/ui/button";
+import { SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+  if (userId != null) redirect("/events");
+
   return (
-    <Button>Hello!</Button>
+    <div className="text-center container py-4 mx-auto">
+      <h1 className="text-3xl mb-4">Fancy Home Page</h1>
+      <div className="flex gap-2 justify-center">
+        <Button asChild>
+          <SignInButton />
+        </Button>
+        <Button asChild>
+          <SignUpButton />
+        </Button>
+        <UserButton />
+      </div>
+    </div>
   );
 }
